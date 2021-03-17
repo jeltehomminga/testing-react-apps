f907dc83a98e82229bdbd4f2aa288f3c4eac0d11
Jelte Homminga <jelte.homminga@gmail.com>
2021-03-17 12:38:59 +0100

    testing

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,22 +5,51 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+// import faker from 'faker'
+import {build, fake} from '@jackfranklin/test-data-bot'
 
 test('submitting the form calls onSubmit with username and password', () => {
   // 🐨 create a variable called "submittedData" and a handleSubmit function that
   // accepts the data and assigns submittedData to the data that was submitted
   // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
+
+  const mockHandleSubmit = jest.fn()
+  render(<Login onSubmit={mockHandleSubmit} />)
   //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
+  // 🐨 get the username and password fields via  `getByLabelText`
+  // const buildLoginForm = (overrides) => ({
+  //   username: faker.internet.userName(),
+  //   password: faker.internet.password(),
+  //   ...overrides
+  // })
+
+  const loginFormBuilder = build('Login', {
+    fields: {
+      username: fake(f => f.internet.userName()),
+      password: fake(f => f.internet.password()),
+    },
+  })
+
+  // const {username, password} = buildLoginForm({username: 'abc'})
+  const {username, password} = loginFormBuilder()
   // 🐨 use userEvent.type to change the username and password fields to
   //    whatever you want
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  userEvent.type(screen.getByLabelText(/password/i), password)
+
   //
   // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
+  userEvent.click(
+    screen.getByRole('button', {
+      name: /submit/i,
+    }),
+  )
+
+  expect(mockHandleSubmit).toHaveBeenCalledWith({
+    username,
+    password,
+  })
+
   // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
 })
 
--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -5,6 +5,8 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
+import {act} from 'react-dom/test-utils'
+import {act as rtlAct, renderHook} from '@testing-library/react-hooks'
 
 // 🐨 create a simple function component that uses the useCounter hook
 // and then exposes some UI that our test can interact with to test the
@@ -12,11 +14,63 @@ import useCounter from '../../components/use-counter'
 // 💰 here's how to use the hook:
 // const {count, increment, decrement} = useCounter()
 
+function CounterComponent() {
+  const {count, increment, decrement} = useCounter({initialCount: 2, step: 3})
+
+  return (
+    <div>
+      <div>Current count: {count}</div>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  )
+}
+
 test('exposes the count and increment/decrement functions', () => {
-  // 🐨 render the component
-  // 🐨 get the elements you need using screen
-  // 🐨 assert on the initial state of the hook
-  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  const {container} = render(<CounterComponent />)
+
+  const decrement = screen.getByRole('button', {name: 'Decrement'})
+  const increment = screen.getByRole('button', {name: 'Increment'})
+  const message = container.firstChild.querySelector('div')
+
+  expect(message).toHaveTextContent('Current count: 2')
+  userEvent.click(increment)
+  expect(message).toHaveTextContent('Current count: 5')
+  userEvent.click(decrement)
+  expect(message).toHaveTextContent('Current count: 2')
+})
+
+function setup(...args) {
+  const result = {}
+  function TestComponent() {
+    result.current = useCounter(...args)
+    return null
+  }
+  render(<TestComponent />)
+  return result
+}
+
+test('exposes the count and increment/decrement functions with setup and returnVal', () => {
+  const result = setup({initialCount: 2, step: 3})
+  expect(result.current.count).toBe(2)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(5)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(2)
+})
+
+test('exposes the count and decrement functions with @testing-library/react-hooks', () => {
+  const {result, rerender} = renderHook(useCounter, {
+    initialProps: {initialCount: 2, step: 3},
+  })
+  expect(result.current.count).toBe(2)
+  rtlAct(() => result.current.increment())
+  expect(result.current.count).toBe(5)
+  rtlAct(() => result.current.decrement())
+  expect(result.current.count).toBe(2)
+  rerender({step: 2})
+  rtlAct(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
 
 /* eslint no-unused-vars:0 */
